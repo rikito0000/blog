fc60eb824c343a3f085a85bb1ba6ab8b2688bedf
EC2 Default User <ec2-user@ip-172-31-61-122.ec2.internal>
2020-07-17 21:22:08 +0000

    add_comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_131433) do
+ActiveRecord::Schema.define(version: 2020_07_17_210251) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_07_17_131433) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "article_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
