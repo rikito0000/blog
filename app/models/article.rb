26441aed775196d630df25052f54353ce8fa9129
EC2 Default User <ec2-user@ip-172-31-61-122.ec2.internal>
2020-08-18 09:19:03 +0000

    cant store

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   validates :title, presence: true, length: { maximum: 50 }
-
+  
+  
   
   belongs_to :user
   has_many :likes, dependent: :destroy
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,21 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  context 'タイトルと内容が入力されている場合' do 
-    before do
-      user = User.create!({
+     let!(:user)do
+      User.create!({
       name: 'test',
       email: 'test@example.com',
       password: 'example'
      })
-      @article = user.articles.build({
+    end
+    
+  context 'タイトルと内容が入力されている場合' do 
+    let!(:user)do
+      User.create!({
+      name: 'test',
+      email: 'test@example.com',
+      password: 'example'
+     })
+    end
+    
+    let!(:article) do 
+      user.articles.build({
       title: Faker::Lorem.characters(number: 30),
       content: Faker::Lorem.characters(number: 255)
      })
     end
+  
     
     it '記事を保存できる' do
-    
+      expect(article).to be_valid
    end
   end
+  
+  context 'タイトルの文字が90字の場合' do
+     let!(:article) do 
+      user.articles.create({
+      title: Faker::Lorem.characters(number: 60),
+      content: Faker::Lorem.characters(number: 255)
+     })
+    end
+    
+    it '記事を保存できない' do
+      expect(article.errors.messages[:title][0]).to eq('is too long (maximum is 50 characters)')
+    end
+  end 
 end
+
